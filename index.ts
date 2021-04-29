e99aad52a515e88b31b3e5490db24212f4e312db
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2021-04-29 19:17:31 +0900

    Rewrite using waitForFunction, reformat with prettier etc.

--- index.ts
+++ index.ts
@@ -1,66 +1,102 @@
-import * as puppeteer from 'puppeteer-core';
-import * as readline from 'readline';
+import * as commandLineArgs from "command-line-args";
+import * as puppeteer from "puppeteer-core";
+import * as readline from "readline";
 
-// The default value is decided by my experiment on my machine.
-const defaultWait = 685;
-let waitFor =
-    process.argv[2] === '--wait' ? parseInt(process.argv[3], 10) : defaultWait;
-if (!waitFor || isNaN(waitFor)) {
-  console.error('[ERROR] Invalid number for --wait option.');
-  process.exit(1);
-}
-console.log(`Configured the --wait option as ${waitFor} milliseconds.`);
+const DEFAULT_CHROMIUM_PATH =
+  "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
 
-const rl = readline.createInterface(
-    {input : process.stdin, output : process.stdout, prompt : '中文> '});
+const optionDefinitions = [
+  { name: "prompt", alias: "p", defaultValue: "中文> ", type: String },
+  {
+    name: "chromium",
+    alias: "c",
+    defaultValue: DEFAULT_CHROMIUM_PATH,
+    type: String,
+  },
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: options.prompt,
+});
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath :
-        'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
-    headless : false,
-    defaultViewport : {height : 600, width : 1000},
+    executablePath: options.chromium,
+    headless: false,
+    defaultViewport: { height: 600, width: 1000 },
   });
   const page = await browser.newPage();
-  await page.goto('https://translate.google.com/?sl=zh-CN&tl=ja&op=translate');
+  await page.goto("https://translate.google.com/?sl=zh-CN&tl=ja&op=translate");
 
   rl.prompt();
-  rl.on('line', async line => {
-      await go(line, page);
-      rl.prompt();
-    }).on('close', async () => {
-    console.log('Bye!');
+  rl.on("line", async (line) => {
+    const result = await getPinyin(line, page);
+    if (result.ok) {
+      console.log(result.body);
+    } else {
+      console.error(result.body);
+    }
+    rl.prompt();
+  }).on("close", async () => {
+    console.log("Bye!");
     await browser.close();
   });
 })();
 
-const go = async (line: string, page: puppeteer.Page) => {
-  line = line.trim();
-  const md = line.match(/^--wait\s+(.*)$/);
-  if (md) {
-    waitFor = parseInt(md[1], 10);
-    if (!waitFor || isNaN(waitFor)) {
-      console.error('[ERROR] Invalid number for waitForwait option.');
-      return;
-    }
-    console.log(`Configured the --wait option as ${waitFor} milliseconds.`);
-    return;
-  }
-  if (!line) {
-    return;
-  }
-  const textarea = await page.$("[aria-label='原文']");
-  await textarea.evaluate(node => node.value = '');
-  await textarea.type(line, {delay : 20});
+type Result = { ok: boolean; body: string };
 
-  const pinyinElement = await page.$('.kO6q6e');
-  await pinyinElement!.focus();
-  await new Promise(resolve => setTimeout(resolve, waitFor));
-  const result =
-      await pinyinElement.evaluate(node => node?.firstChild?.nodeValue);
-  if (!result) {
-    console.error('Pinyin element not found!');
-    return;
+const getPinyin = async (
+  line: string,
+  page: puppeteer.Page
+): Promise<Result> => {
+  const input = line.trim();
+  if (!input) {
+    return { ok: false, body: "Empty input" };
   }
-  console.log(result);
-}
+
+  const pinyinElementClass = "kO6q6e";
+
+  // Clear the input once to avooid getting a stale result unexpectedly.
+  try {
+    await page.click("[aria-label='原文を消去']");
+  } catch (e) {
+    if (e.message !== "Node is either not visible or not an HTMLElement") {
+      throw e;
+    }
+  }
+  await page.waitForFunction(
+    (selector: string) => {
+      const nodes = document.getElementsByClassName(selector);
+      const content = nodes[0]?.firstChild?.nodeValue;
+      return !content;
+    },
+    { polling: "mutation" },
+    pinyinElementClass
+  );
+
+  const textarea = await page.$("[aria-label='原文']");
+  await textarea.type(line);
+
+  try {
+    const resultHandle = await page.waitForFunction(
+      (selector: string) => {
+        const nodes = document.getElementsByClassName(selector);
+        return nodes[0]?.firstChild?.nodeValue;
+      },
+      { polling: "mutation" },
+      pinyinElementClass
+    );
+    const result = (await resultHandle.jsonValue()) as string;
+    return { ok: true, body: result };
+  } catch (e) {
+    if (e instanceof puppeteer.errors.TimeoutError) {
+      const body = "Failed to get pinyin! Is this really a Chinese word?";
+      return { ok: false, body };
+    }
+    throw e;
+  }
+};
